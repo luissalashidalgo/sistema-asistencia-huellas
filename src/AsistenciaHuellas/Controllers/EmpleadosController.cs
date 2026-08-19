8fcf0041e250183cea55e6b7a14fd8cc4e28df12
Luis Salas <luissalash@gmail.com>
2026-08-19 10:33:40 -0600

    Se crea el controller Empleados

---
+++ src/AsistenciaHuellas/Controllers/EmpleadosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AsistenciaHuellas.Controllers
+{
+    [ApiController]
+    [Route("api/Empleados")]
+    public class EmpleadosController : ControllerBase
+    {
+
+    }
+}
